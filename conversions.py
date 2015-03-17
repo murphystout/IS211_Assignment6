f8acdf828aa3fb17a8ac6f03eb814c7b75e3f95d
murphystout <hayes_michael_d@yahoo.com>
2015-03-17 12:46:20 -0400

    Second Commit - Initial test units working

--- conversions.py
+++ conversions.py
@@ -3,9 +3,17 @@ class NonNumericInputError(Exception): pass
 class LowerLimitError(Exception): pass
 
 def convertCelciustoFahrenheit(input):
-	return float(input * (9/5) + 32)
+	if (not type(input) == int) and (not type(input) == float) and (not type(input) == long):
+		raise NonNumericInputError, 'Non Numeric Input:'
+	if input < -273.15:
+		raise LowerLimitError, 'Lower Limit Error. Input less than 273.15'
+	return round(input * 9.0/5.0 + 32.0,2)
 
 def convertCelsiustoKelvin(input):
-	return float(input + 273.15)
-
+	if (not type(input) == int) and (not type(input) == float) and (not type(input) == long):
+		raise NonNumericInputError, 'Non Numeric Input:'
+	elif input < -273.15:
+		raise LowerLimitError, 'Lower Limit Error. Input less than 273.15'
+	else:
+		return round(input + 273.15,2)
 	
--- tests.py
+++ tests.py
@@ -3,54 +3,44 @@ import unittest
 
 
 class CelctoFarKnown(unittest.TestCase):
-	knownvalues = ( (0,32),(5,41),(7,44.6),(13,55.4),(16,60.8),(26,78.8),(29,84.2),(37,98.6),(52,125.6),(100,212),(138,280.4),(142,287.6),(145,293),(210,410))
+	knownvalues = ( (0,32),(7,44.6),(13,55.4),(16,60.8),(26,78.8),(29,84.2),(37,98.6),(52,125.6),(100,212),(138,280.4),(142,287.6),(145,293),(210,410))
 	
 	def testToFarKnownValues(self):
+		"""CelctoFar should succeed for all known inputs"""
 		for celc, far in self.knownvalues:
 			result = conversions.convertCelciustoFahrenheit(celc)
 			self.assertEqual(far, result)
 	
 class CelctoFarBadInput(unittest.TestCase):
 	def testNonNumeric(self):
+		"""CelctoFar should fail with non-numeric input"""
 		badinputs = ["Text String", ("Text","Tuple"), ["Text", "List"],True,False,None]
 		for member in badinputs:
 			self.assertRaises(conversions.NonNumericInputError, conversions.convertCelciustoFahrenheit, member)
-	
+
 	def testLowerLimit(self):
+		"""Celftofar should fail on input less than absolute zero"""
 		self.assertRaises(conversions.LowerLimitError, conversions.convertCelciustoFahrenheit, -300)
 		
-
 class CelctoKelvinKnown(unittest.TestCase):
 	knownvalues = ( (-273.15, 0), (-260,13.15), (-200,73.15), (-160, 113.15), (-100, 173.15), (0,273.15),(50,323.15),(100,373.15),(200,473.15),(250,523.15),(300,573.15))
 	def testToKelvinKnownValues(self):
+		"""CelctoKelvin should succeed on all known inputs"""
 		for celc, kelv in self.knownvalues:
 			result = conversions.convertCelsiustoKelvin(celc)
 			self.assertEqual(kelv, result)
 
 class CelctoKelvinBadInput(unittest.TestCase):
 	def testNonNumeric(self):
+		"""CelctoKelvin should fail on non-numeric input"""
 		badinputs = ["Text String", ("Text","Tuple"), ["Text", "List"],True,False,None]
 		for member in badinputs:
 			self.assertRaises(conversions.NonNumericInputError, conversions.convertCelsiustoKelvin, member)
 			
 	def testLowerLimit(self):
-		self.assertRaises(conversions.LowerLimitError, conversions.convertCelsiustoKelvin, -1)
+		"""CelctoKelvin should fail on input less than absolute zero"""
+		self.assertRaises(conversions.LowerLimitError, conversions.convertCelsiustoKelvin, -300)
 	
 if __name__ == '__main__':
     unittest.main()
-			
 	
-
-
-
-
-
-
-
-
-
-
-
-
-
-		
