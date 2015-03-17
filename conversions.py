0f1abb515a07353ead80bc75c65912c9e54e5a47
murphystout <hayes_michael_d@yahoo.com>
2015-03-17 13:55:35 -0400

    Third Commit, All Functions and Tests Working

--- conversions.py
+++ conversions.py
@@ -6,14 +6,48 @@ def convertCelciustoFahrenheit(input):
 	if (not type(input) == int) and (not type(input) == float) and (not type(input) == long):
 		raise NonNumericInputError, 'Non Numeric Input:'
 	if input < -273.15:
-		raise LowerLimitError, 'Lower Limit Error. Input less than 273.15'
-	return round(input * 9.0/5.0 + 32.0,2)
+		raise LowerLimitError, 'Lower Limit Error. Celcius Input less than -273.15'
+	else:
+		return round(input * 9.0/5.0 + 32.0,2)
 
 def convertCelsiustoKelvin(input):
 	if (not type(input) == int) and (not type(input) == float) and (not type(input) == long):
 		raise NonNumericInputError, 'Non Numeric Input:'
 	elif input < -273.15:
-		raise LowerLimitError, 'Lower Limit Error. Input less than 273.15'
+		raise LowerLimitError, 'Lower Limit Error. Celcius Input less than -273.15'
 	else:
 		return round(input + 273.15,2)
 	
+def convertFahrenheittoCelcius(input):
+	if (not type(input) == int) and (not type(input) == float) and (not type(input) == long):
+		raise NonNumericInputError, 'Non Numeric Input:'
+	if input < -459.67:
+		raise LowerLimitError, 'Lower Limit Error. Fahrenheit Input less than -459.67'
+	else:
+		return round((input - 32.0) * 5.0/9.0,2)
+
+def convertFahrenheittoKelvin(input):
+	if (not type(input) == int) and (not type(input) == float) and (not type(input) == long):
+		raise NonNumericInputError, 'Non Numeric Input:'
+	if input < -459.67:
+		raise LowerLimitError, 'Lower Limit Error. Fahrenheit Input less than -459.67'
+	else:
+		return round((input + 459.67) * 5/9,2)
+	
+		
+def convertKelvintoCelcius(input):
+	if (not type(input) == int) and (not type(input) == float) and (not type(input) == long):
+		raise NonNumericInputError, 'Non Numeric Input:'
+	if input < 0:
+		raise LowerLimitError, 'Lower Limit Error. Kelvin Input less than 0'
+	else:
+		return round(input - 273.15,2)
+	
+def convertKelvintoFahrenheit(input):
+	if (not type(input) == int) and (not type(input) == float) and (not type(input) == long):
+		raise NonNumericInputError, 'Non Numeric Input:'
+	if input < 0:
+		raise LowerLimitError, 'Lower Limit Error. Kelvin Input less than 0'
+	else:
+		return round(input * 9.0/5.0 - 459.67,2)
+		
--- tests.py
+++ tests.py
@@ -41,6 +41,80 @@ class CelctoKelvinBadInput(unittest.TestCase):
 		"""CelctoKelvin should fail on input less than absolute zero"""
 		self.assertRaises(conversions.LowerLimitError, conversions.convertCelsiustoKelvin, -300)
 	
+class FahrenheittoCelciusKnown(unittest.TestCase):
+	knownvalues = ((-459.67,-273.15),(-292.00,-180.00), (-94.00, -70.00), (-4.00, -20.00), (32.00, 0.00), (86.00, 30.00), (176.00, 80.00),(284.00,140.00), (572.00, 300))
+	def testFahrenheittoCelciusValues(self):
+		"""CelctoKelvin should succeed on all known inputs"""
+		for input, output in self.knownvalues:
+			result = conversions.convertFahrenheittoCelcius(input)
+			self.assertEqual(result, output)
+		
+class FahrenheittoCelciusBadInput(unittest.TestCase):
+	def testNonNumeric(self):
+		"""FahrenheittoCelcius should fail on non-numeric input"""
+		badinputs = ["Text String", ("Text","Tuple"), ["Text", "List"],True,False,None]
+		for member in badinputs:
+			self.assertRaises(conversions.NonNumericInputError, conversions.convertFahrenheittoCelcius, member)
+	def testLowerLimit(self):
+		"""FahrenheittoCelcius should fail on input less than absolute zero"""
+		self.assertRaises(conversions.LowerLimitError, conversions.convertFahrenheittoCelcius, -500)	
+
+class FahrenheittoKelvinKnown(unittest.TestCase):
+	knownvalues = ((-459.67,0.00),(-364.00,53.15),(-202.00, 143.15), (-130.00, 183.15) ,(-22.00, 243.15), (86.00, 303.15), (212.00, 373.15), (392.00,473.15))
+	def testFahrenheittoKelvinValues(self):
+		
+		"""FahrenheittoKelvin should succeed on all known inputs"""
+		for input, output in self.knownvalues:
+			result = conversions.convertFahrenheittoKelvin(input)
+			self.assertEqual(result, output)
+			
+class FahrenheittoKelvinBadInput(unittest.TestCase):
+	def testNonNumeric(self):
+		"""FahrenheittoKelvin should fail on non-numeric input"""
+		badinputs = ["Text String", ("Text","Tuple"), ["Text", "List"],True,False,None]
+		for member in badinputs:
+			self.assertRaises(conversions.NonNumericInputError, conversions.convertFahrenheittoKelvin, member)
+	def testLowerLimit(self):
+		"""FahrenheittoKelvin should fail on input less than absolute zero"""
+		self.assertRaises(conversions.LowerLimitError, conversions.convertFahrenheittoKelvin, -500)
+		
+class KelvintoCelciusKnown(unittest.TestCase):
+	knownvalues = ((0.00, -273.15), (63.15,-210.00), (123.15, -150.00), (193.15, -80.00), (243.15, -30.00), (323.15, 50), (393.15, 120.00), (483.15, 210.00))
+	def testKelvintoCelciusValues(self):
+		"""KelvintoCelcius should succeed on all known inputs"""
+		for input, output in self.knownvalues:
+			result = conversions.convertKelvintoCelcius(input)
+			self.assertEqual(result, output)
+			
+class KelvintoCelciusBadInput(unittest.TestCase):
+	def testNonNumeric(self):
+		"""KelvintoCelcius should fail on non-numeric input"""
+		badinputs = ["Text String", ("Text","Tuple"), ["Text", "List"],True,False,None]
+		for member in badinputs:
+			self.assertRaises(conversions.NonNumericInputError, conversions.convertKelvintoCelcius, member)
+	def testLowerLimit(self):
+		"""KelvintoCelcius should fail on input less than absolute zero"""
+		self.assertRaises(conversions.LowerLimitError, conversions.convertKelvintoCelcius, -1)	
+		
+class KelvintoFahrenheitKnown(unittest.TestCase):
+	knownvalues = ((0.00, -459.67), (63.15,-346.00), (123.15, -238.00), (193.15, -112.00), (243.15, -22.00), (323.15, 122.00), (393.15, 248.00), (483.15, 410.00))
+	def testKelvintoFahrenheitValues(self):
+		"""KelvintoFahrenheit should succeed on all known inputs"""
+		for input, output in self.knownvalues:
+			result = conversions.convertKelvintoFahrenheit(input)
+			self.assertEqual(result, output)
+			
+class KelvintoFahrenheitBadInput(unittest.TestCase):
+	def testNonNumeric(self):
+		"""KelvintoFahrenheit should fail on non-numeric input"""
+		badinputs = ["Text String", ("Text","Tuple"), ["Text", "List"],True,False,None]
+		for member in badinputs:
+			self.assertRaises(conversions.NonNumericInputError, conversions.convertKelvintoFahrenheit, member)
+	def testLowerLimit(self):
+		"""KelvintoFahrenheit should fail on input less than absolute zero"""
+		self.assertRaises(conversions.LowerLimitError, conversions.convertKelvintoFahrenheit, -1)	
+
+
 if __name__ == '__main__':
     unittest.main()
 	
